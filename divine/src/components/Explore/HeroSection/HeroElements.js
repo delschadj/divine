83fd940d7c7ad4ea65313e042a0cc14d5755e608
delschadj <delschad.jankir@gmx.de>
2022-07-30 04:43:04 +0200

    Hero section done

--- divine/src/components/Explore/HeroSection/HeroElements.js
+++ divine/src/components/Explore/HeroSection/HeroElements.js
@@ -5,10 +5,24 @@ export const HeroContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 0 30px;
+    padding: 0px;
     height: 800px;
     position: relative;
     z-index: 1
+
+    :before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: linear-gradient (180deg, 
+        rgba(0,0,0,0.2) 0%),
+        rgba (0,0,0,0.6) 100%),
+        linear-gradient (180def, rgba(0,0,0,0.2) 0%, transparent 100%);
+        z-index: 2;
+    }
 `
 
 export const HeroBg = styled.div`
@@ -29,7 +43,6 @@ export const ImageBg = styled.video`
     object-fit: cover;
     background: #232a34;
 `
-
 
 export const HeroContent = styled.div`
     z-index: 3;
--- divine/src/components/Explore/HeroSection/index.js
+++ divine/src/components/Explore/HeroSection/index.js
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Video from "../../../assets/videos/video.mp4"
-import header from "../../../assets/images/header.jpeg"
+import Video2 from "../../../assets/videos/video2.mp4"
 
+import { Button } from '../../ButtonElement'
 
 
 import { HeroContainer, HeroBg, ImageBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper} from "./HeroElements"
 
 function HeroSection() {
+
+    const [hover, setHover] = useState (false);
+
+    const onHover = () => {
+        setHover (!hover);
+    }
+
+
   return (
     <HeroContainer>
         <HeroBg>
-            <ImageBg autoPlay loop muted src={Video} type="video/mp4"/>
+            <ImageBg src={Video2}/>
         </HeroBg>
 
         <HeroContent>
-            <HeroH1>Virtual Banking Made Easy</HeroH1>
+            <HeroH1>Start exploring the world of other religions.</HeroH1>
             <HeroP>
-                Sign up for a new account today and recieve 250€
-                in credit towards your next payment.
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+            sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+            magna aliquyam erat, sed diam voluptua. 
+            At vero eos et accusam et justo duo dolores et ea rebum. 
             </HeroP>
             <HeroBtnWrapper>
-                
+                <Button to="/account" 
+                onMouseEnter={onHover} 
+                onMouseLeave={onHover}
+                primary="true"
+                dark="true"
+                >
+                    Button 
+                </Button>
             </HeroBtnWrapper>
         </HeroContent>
 
